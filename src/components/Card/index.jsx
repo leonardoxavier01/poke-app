f303afed764cc8ee9851a51534ea95bf9ec09984
unknown <leonardoxavier092@gmail.com>
2022-03-26 23:29:24 -0300

    test: get pokemon, exit map

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,39 +5,39 @@
 const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`
 
 export function filterPokemons(filter) {
-  console.log(filter)
-  console.log(pokemons)
-  let resultPokemon = pokemons.filter(p => p.name.includes(filter))
-  let htmlResults = generateHTML(resultPokemon)
-  insertPokemonsIntoPage(htmlResults)
+ console.log(filter)
+ console.log(pokemons)
+ let resultPokemon = pokemons.filter(p => p.name.includes(filter))
+ let htmlResults = generateHTML(resultPokemon)
+ insertPokemonsIntoPage(htmlResults)
 }
 
 var pokemons = new Array(12);
 
 const generatePokemonPromises = () =>
-  pokemons.fill()
-    .map((_, index) =>
-      fetch(getPokemonUrl(index + 1)).then((response) => response.json())
-    )
+ pokemons.fill()
+   .map((_, index) =>
+     fetch(getPokemonUrl(index + 1)).then((response) => response.json())
+   )
 
 const generateHTML = (pokemons) =>
-  pokemons.reduce((accumulator, { name, id, types }) => {
-    const elementTypes = types.map((typeInfo) => typeInfo.type.name)
+ pokemons.reduce((accumulator, { name, id, types }) => {
+   const elementTypes = types.map((typeInfo) => typeInfo.type.name)
 
-    accumulator += `
-    <li className= 'card ${elementTypes[0]}'>
-    <img className= 'cardImage' alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png" />
-    <h2 className='cardTitle'> ${name} </h2>
-    <p className='cardSubtitle'>${elementTypes.join(' | ')} </p>
-    <Button>See More</Button>
-    </li>
-    `
-    return accumulator
-  }, '')
+   accumulator += `
+   <li className= 'card ${elementTypes[0]}'>
+   <img className= 'cardImage' alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png" />
+   <h2 className='cardTitle'> ${name} </h2>
+   <p className='cardSubtitle'>${elementTypes.join(' | ')} </p>
+   <Button>See More</Button>
+   </li>
+   `
+   return accumulator
+ }, '')
 
 const insertPokemonsIntoPage = (pokemons) => {
-  const ul = document.querySelector('[data-js= "pokedex"]')
-  ul.innerHTML = pokemons
+ const ul = document.querySelector('[data-js= "pokedex"]')
+ ul.innerHTML = pokemons
 }
 
 const pokemonPromises = generatePokemonPromises()
@@ -45,14 +45,31 @@ const pokemonPromises = generatePokemonPromises()
 Promise.all(pokemonPromises).then(generateHTML).then(insertPokemonsIntoPage)
 
 const Card = () => (
-  <>
-    <PokemonUl data-js="pokedex" className="pokedex"></PokemonUl>
-  </>
+ <>
+   <PokemonUl data-js="pokedex" className="pokedex"></PokemonUl>
+ </>
 )
 
-export default Card  */
+export default Card   */
 
-import { render } from '@testing-library/react'
+
+
+/*  async poke(pokemons) {
+    pokemons.map(pokemon => {
+      axios.create({
+        method: 'get', url: pokemon.url
+      }).then(function (response) {
+        console.log()
+      })
+
+    })
+  } */
+
+
+
+
+
+
 import React, { Component } from 'react'
 import api from '../api'
 
@@ -62,34 +79,40 @@ class Card extends Component {
 
   state = {
     pokemons: [],
+
   }
 
   async componentDidMount() {
-    const response = await api.get('/pokemon?limit=10&offset=0');
-
-    /*  console.log(response.data.results)  */
-
-    this.setState({ pokemons: response.data.results });
+    const id = 5;
+    const response = await api.get(`/pokemon/${id}/`);
 
 
+    this.setState({ pokemons: response.data });
+    console.log(response.data)
   }
 
+
+
+
+
+
+
   render() {
-
     const { pokemons } = this.state;
-
-    console.log(pokemons); 
-
-
+    const pokemon = pokemons
+    console.log(pokemons);
     return (
       <div>
         <h1>Teste pokemon</h1>
         <ul>
-          {pokemons.map(pokemon => (
-            <li>
-              <h2>Titulo:{pokemon.name} </h2>
-            </li>
-          ))}
+
+          <li>
+            <p>{pokemon.id}</p>
+            <h2>{pokemon.name}</h2>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png`} alt={pokemon.name} />
+            <p></p>
+          </li>
+
         </ul>
       </div>
     );
