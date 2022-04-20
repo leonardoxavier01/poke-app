94443b5a5b22886040ce6cdd776d536677f11577
unknown <leonardoxavier092@gmail.com>
2022-04-20 20:23:50 -0300

    style: background in var

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -7,12 +7,6 @@ export const FooterContainer = styled.footer`
   justify-content: center;
   align-items: center;
   height: 15.43rem;
-  background: rgb(120, 86, 254);
-  background: linear-gradient(
-    90deg,
-    rgba(120, 86, 254, 1) 0%,
-    rgba(193, 69, 175, 1) 57%
-  );
 `
 
 export const LogoAwari = styled.img`
