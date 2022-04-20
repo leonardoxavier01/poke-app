7cd7c417d0b0c132954cfcddc97cced08ec96426
unknown <leonardoxavier092@gmail.com>
2022-04-20 20:50:27 -0300

    feat: searchr pokemon in inputSearch

--- src/components/PokeList/index.jsx
+++ src/components/PokeList/index.jsx
@@ -1,50 +1,77 @@
 import React, { useState, useEffect } from 'react'
 import { PokeListContainer, PokeListWrapper } from './styles'
 import Card from '../Card'
+import InputSearch from '../InputSearch'
+import baseUrl from '../../Services/pokeApiAxios'
 
 const PokeList = () => {
-
   const [allPokemons, setAllPokemons] = useState([])
-  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
-
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=10')
+  const [input, setInput] = useState('')
+  
   const getAllPokemons = async () => {
     const res = await fetch(loadMore)
     const data = await res.json()
-
     setLoadMore(data.next)
-
     function createPokemonObject(results) {
       results.forEach(async pokemon => {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const res = await fetch(pokemon.url)
         const data = await res.json()
-        setAllPokemons(currentList => [...currentList, data])
-        await allPokemons.sort((a, b) => a.id - b.id)
+        await cretePokemon(data)
       })
     }
     createPokemonObject(data.results)
+  }
+
+  const cretePokemonSearch = async (res) => {
+    setAllPokemons([])
+    await cretePokemon(res)
+  }
+
+  const cretePokemon = async (res) => {
+    setAllPokemons(currentList => [...currentList, res])
+    await allPokemons.sort((a, b) => a.id - b.id)
   }
 
   useEffect(() => {
     getAllPokemons()
   }, [])
 
+  async function handleSearch() {
+    if (input === '') {
+      location.reload();
+    }
+    try {
+      const inputPokemon = input.toLowerCase();
+      const response = await baseUrl.get(`/pokemon/${inputPokemon}`)
+      cretePokemonSearch(response.data)
+      setInput(input)
+    } catch {
+      alert('Digite um nome correto!')
+      setInput('')
+    }
+  }
+
   return (
     <PokeListContainer>
-      <div className="app-contaner">   
-        <div className="pokemon-container">
-          <PokeListWrapper className="all-container">
-            {allPokemons.map((pokemonStats, index) =>
-              <Card
-                key={index}
-                id={pokemonStats.id}
-                image={pokemonStats.sprites.other.dream_world.front_default}
-                name={pokemonStats.name}
-                type={pokemonStats.types[0].type.name}
-              />)}
-          </PokeListWrapper>
-          <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
-        </div>
+      <div>
+        <InputSearch
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onClick={handleSearch}
+        />
+        <PokeListWrapper className="all-container">
+          {allPokemons.map((pokemonStats, index) =>
+            <Card
+              key={index}
+              id={pokemonStats.id}
+              image={pokemonStats.sprites.other.dream_world.front_default}
+              name={pokemonStats.name}
+              type={pokemonStats.types[0].type.name}
+            />)}
+        </PokeListWrapper>
       </div>
+
     </PokeListContainer>)
 }
 
