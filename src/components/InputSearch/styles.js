3eb74107e8d22fa75766805f04ca148a40463c69
unknown <leonardoxavier092@gmail.com>
2022-05-30 19:25:17 -0300

    test git

--- src/components/InputSearch/styles.js
+++ src/components/InputSearch/styles.js
@@ -9,7 +9,6 @@ export const SearchContainer = styled.div`
   box-shadow: 1px 3px 8px rgba(0, 0, 0, 0.5);
   width: 29rem;
   border: 2px solid ${({ theme }) => theme.lightGrey};
-  
   input {
     background-color: transparent;
     border: 0;
