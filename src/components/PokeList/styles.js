191155e3e8e9ec2700385d98b0bc9a7818ae53bf
unknown <leonardoxavier092@gmail.com>
2022-05-30 13:04:54 -0300

    style: grid responsive

--- src/components/PokeList/styles.js
+++ src/components/PokeList/styles.js
@@ -4,9 +4,14 @@ export const PokeListWrapper = styled.div`
 padding: 2rem;
 margin-top: 3rem;
 display: grid;
-grid-template-columns: repeat(3, 1fr);
 grid-column-gap: 32px;
 grid-row-gap: 52px;
-border-radius: 50px 50px 50px 150px;
+border-radius: 50px 50px 50px 50px;
 border: 0.4rem solid #A2A3A3;
+@media (min-width: 600px) {
+   grid-template-columns: repeat(2, 1fr); 
+}
+@media (min-width: 900px) {
+   grid-template-columns: repeat(3, 1fr);
+}
 `
