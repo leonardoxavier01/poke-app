c60a33a408c526ed65f5060c4e4647780b0aa489
unknown <leonardoxavier092@gmail.com>
2022-06-15 16:00:14 -0300

    feat:styled components implementation

--- src/components/PokeList/styles.js
+++ src/components/PokeList/styles.js
@@ -5,7 +5,7 @@ padding: 2rem;
 margin-top: 3rem;
 display: grid;
 grid-column-gap: 32px;
-grid-row-gap: 52px;
+grid-row-gap: 30px;
 border-radius: 50px 50px 50px 50px;
 border: 0.3rem solid  rgba(193, 69, 175, 1) ;
 @media (min-width: 600px) {
