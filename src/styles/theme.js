a59e7ac22b785a33d9e0095660ea983239f1ec88
unknown <leonardoxavier092@gmail.com>
2022-06-21 15:02:57 -0300

    perf:create const themePokemons

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,24 +1,35 @@
 export const theme = {
   black: '#121212',
   blackout: '#202020',
-  Grey: '#333333',
+  grey: '#333333',
   darkGrey: '#8c8c8d',
   lightGrey: '#c9cad1',
   white: '#fff',
-  offWhite:'#ffffff7a',
-  secondaryWhite:'#ffffffa4',
-  primary: '#44cca6',
-  grass: '#74cb48',
-  bug: '#a7b723',
-  fire: '#f57d31',
-  psychic: '#fb5584',
-  poison: '#a43e9e',
-  ghost: '#70559b',
-  flying: '#a891ec',
-  water: '#6493eb',
-  dark: '#75574c',
-  backgroundGradient: 'linear-gradient(90deg,rgba(120, 86, 254, 1) 0%, rgba(193, 69, 175, 1) 57%)',
-  
+  offWhite: '#ffffff7a',
+  secondaryWhite: '#ffffffa4',
+  backgroundGradient:
+    'linear-gradient(90deg,rgba(120, 86, 254, 1) 0%, rgba(193, 69, 175, 1) 57%)',
+
   pxToRem: (px) => `${px / 16}rem`,
-  
+}
+
+export const themePokemons = {
+  bug: '#A7B723',
+  dark: '#75574C',
+  dragon: '#3e7746',
+  electric: '#ded237',
+  fairy: '#f396c9',
+  fighting: '#f0a715',
+  fire: '#e1521a',
+  flying: '#A891EC',
+  ghost: '#70559B',
+  grass: '#45be41',
+  ground: '#f09d3e',
+  ice: '#3addec',
+  normal: '#667e79',
+  poison: '#A43E9E',
+  psychic: '#b93057',
+  rock: '#8d5e0f',
+  steel: '#1c566f',
+  water: '#6493EB',
 }
