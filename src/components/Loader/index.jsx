a376180ff76e7a7eb917d2be69c65eb7d218068c
unknown <leonardoxavier092@gmail.com>
2022-07-20 13:03:23 -0300

    feat: new loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,11 +1,9 @@
-import pokeball from '../../assets/images/pokeball.png'
 import { Container } from './styles'
 
 const Loader = () => {
   return (
     <Container>
-      <img src='https://cdn-icons-png.flaticon.com/512/188/188918.png' alt="pokeball image" />
-      <h1>Loading...</h1>
+      <div></div>
     </Container>
   )
 }
--- src/components/Loader/styles.js
+++ src/components/Loader/styles.js
@@ -1,22 +1,33 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Container = styled.div`
-width: 100%;
-height: 100vh;
-display: flex;
-align-items: center;
-justify-content: center;
-flex-direction: column;
-img{
-    width: 8rem;
-    animation: rotateCW 1.5s infinite linear;
-}
-h1{
-    font-size: 1.5rem;
-    color:${({theme})=> theme.blackout};
-}
-@keyframes rotateCW {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  div {
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    ::after {
+      content: ' ';
+      display: block;
+      width: 64px;
+      height: 64px;
+      margin: 8px;
+      border-radius: 50%;
+      border: 6px solid #fff;
+      border-color: #fff transparent #fff transparent;
+      animation: lds-dual-ring 1.2s linear infinite;
+    }
+  }
+  @keyframes lds-dual-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `
